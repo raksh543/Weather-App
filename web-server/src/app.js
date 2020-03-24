8c1930583a6712154186c6a0d6f400b7d48e9ab3
raksh543 <rakshitajain777@gmail.com>
2020-03-24 16:35:31 +0530

    initial commit

---
+++ web-server/src/app.js
@@ -0,0 +1,96 @@
+const path=require('path')
+const express=require('express')
+const hbs=require('hbs')
+
+// console.log(__dirname)
+// console.log(path.join(__dirname,'../public')) 
+
+const app=express()
+const publicDirectoryPath=path.join(__dirname,'../public')
+const viewPath=path.join(__dirname,'../templates/views')
+const partialsPath=path.join(__dirname,'../templates/partials')
+
+//setting up handlers
+app.set('view engine','hbs')
+
+//setting path for views
+app.set('views',viewPath)
+
+hbs.registerPartials(partialsPath)
+
+//app.use is a way to customize your server
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title: 'Weather',
+        name: 'Rakshita Jain '
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        name:'Rakshita Jain',
+        title:'About me'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helpText: 'This is some helpful text',
+        title:'Help',
+        name: 'Rakshita Jain'
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:'Rakshita Jain',
+        errorMessage:'Help article not found'
+    })
+})
+//using wild charachter * to match everything that haven't been matched so far
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:'Rakshita Jain',
+        errorMessage:'Page not found'
+    })
+})
+
+//what server should do when a client is accessing a resource
+//This get method takes in two arguements(route,function)
+
+// app.get('',(req,res)=>{
+//     res.send('<h1>Hello express</h1>')
+// })
+
+// app.get('/help',(req,res)=>{
+//     res.send([{
+//         name:'Andrew',
+//         age:27
+//     },{
+//         name:'sarah'
+//     }])
+// })
+// app.get('/about',(req,res)=>{
+//     res.send('<h1>About</h1>')
+// })
+// app.get('/weather',(req,res)=>{
+//     res.send({
+//         forecast:'It is snowing',
+//         location:'Philadelphia'
+//     })
+// })
+
+
+//app.com
+//app.com/help
+//app.com/about
+
+//To start the server
+//port 3000 is for local developement environment
+app.listen(3000,()=>{
+    console.log('Server is up on the port 3000')//This msg is never gonna display
+})
